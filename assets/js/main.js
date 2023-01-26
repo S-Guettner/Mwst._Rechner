7d900749373fccb7af7cc0185f66586f72e96182
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-26 13:29:27 +0100

    basic setup

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+const nettoToBrutto = document.querySelector("#nettoToBrutto")
+const BruttoToNetto = document.querySelector("#BruttoToNetto")
+
+const steuerbetragOutput = document.querySelector("#steuerbetragOutput")
+const endpreisOutput = document.querySelector("#endpreisOutput")
+
+const preisArtOutput = document.querySelector("#preisArtOutput")
+
+let contentChange = () =>{
+    if(Number(nettoToBrutto.value) === 1){
+        preisArtOutput.innerHTML = "Nettobetrag (Preis ohne Mehrwertsteuer) in Euro"
+    }else{
+        preisArtOutput.innerHTML = "Bruttobetrag(Preis inclusive Mehrwertsteuer) in Euro"
+    }
+}
+contentChange();
