e774d54f0712e62eb2217c1b310cb9f2fdf02be6
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-26 21:39:22 +0100

    clean up

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const endpreisArtOutput = document.querySelector("#endpreisArtOutput")
 const preisArtOutput = document.querySelector("#preisArtOutput")
 const endpreisOutput = document.querySelector("#endpreisOutput")
 const nettoToBrutto = document.querySelector("#nettoToBrutto")
-const BruttoToNetto = document.querySelector("#BruttoToNetto")
+const bruttoToNetto = document.querySelector("#BruttoToNetto")
 const priceInput = document.querySelector("#priceInput")
 const prozent19 = document.querySelector("#prozent19")
 const prozent7 = document.querySelector("#prozent7")
@@ -18,13 +18,13 @@ const contentChange = () =>{
     }
 }
 const calculate = () =>{
-    if(nettoToBrutto.checked === true && prozent19.checked == true){
+    if(nettoToBrutto.checked === true && prozent19.checked === true){
         steuerbetragOutput.innerHTML = (priceInput.value * 0.19).toFixed(2)
         endpreisOutput.innerHTML = (priceInput.value * 1.19).toFixed(2)
     }else if(nettoToBrutto.checked === true && prozent7.checked === true){
         steuerbetragOutput.innerHTML = (priceInput.value * 0.07).toFixed(2)
         endpreisOutput.innerHTML = (priceInput.value * 1.07).toFixed(2)
-    }else if(BruttoToNetto.checked === true && prozent19.checked === true){
+    }else if(bruttoToNetto.checked === true && prozent19.checked === true){
         steuerbetragOutput.innerHTML = ((priceInput.value) - (priceInput.value / 1.19)).toFixed(2)
         endpreisOutput.innerHTML = priceInput.value - (((priceInput.value) - (priceInput.value / 1.19)).toFixed(2))
     }else{
